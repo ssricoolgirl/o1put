c2366cf99fd896abc9ce75260ff2bdeb071cacb5
ssricoolgirl <102844143+ssricoolgirl@users.noreply.github.com>
2023-09-03 22:06:35 -0400

    Added react hooks

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,9 @@
+import React from "react";
 const Header = ()=>{
-    // const headerStyle={
-    //     backgroundColor: "mediumblue",
-    //     color: '#fff'
-    // }
+     const headerStyle={
+         backgroundColor: "mediumblue",
+        color: '#fff'
+     }
     return (
         <header style={headerStyle}>
                 <h1> Groceries List </h1>
